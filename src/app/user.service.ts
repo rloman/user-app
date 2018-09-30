19554fd8e2bd0beb38f63c490d410848d85e9071
Raymond Loman <raymond@carpago.nl>
2018-09-30 09:51:25 +0200

    Add user service and consequences

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { User } from './user';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private url: string = "http://jsonplaceholder.typicode.com"; // this will change during dev, test, acct and production environment
+
+  private endpoint: string = "users";
+
+  // private httpClient: HttpClient;
+
+   constructor(private httpClient: HttpClient) { // this result in the (commented) field above ... 
+
+   }
+
+  get(): Observable<User[]> {
+    return this.httpClient.get<User[]>(`${this.url}/${this.endpoint}`);
+  }
 }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  private users: User[];
+
+	// dependency injects the userService here as a field / instance var
+  constructor(private userService: UserService) {
+
+   }
 
   ngOnInit() {
+     this.userService.get().subscribe(users => {
+      this.users = users;
+    });
+
+
   }
 
 }
