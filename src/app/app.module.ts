22537c36ae03186a91309cfbc82bf6c9ff54301b
Raymond Loman <raymond@carpago.nl>
2018-09-30 09:59:32 +0200

    Add skeleton for user-create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { HttpClientModule } from '@angular/common/http';
+import { UserCreateComponent } from './user-create/user-create.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    UserCreateComponent
   ],
   imports: [
     BrowserModule,
